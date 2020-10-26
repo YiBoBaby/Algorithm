0ec4f9d72fa88c94ae21ca42fc7a71ba7885187d
YiBoBaby <2994219729@qq.com>
2020-10-27 00:35:34 +0800

    One question per day

--- leetcode/tree/0094.go
+++ leetcode/tree/0094.go
@@ -1,11 +1,5 @@
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
 func main() {
 
 }
---
+++ leetcode/tree/0144.go
@@ -0,0 +1,33 @@
+package main
+
+import "container/list"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	stack := list.New()
+	node := root
+	res := make([]int, 0, 0)
+
+	for stack.Len() > 0 || node != nil {
+		if node != nil {
+			stack.PushBack(node)
+			res = append(res, node.Val)
+			node = node.Left
+		} else {
+			ele := stack.Back()
+			if v, ok := ele.Value.(*TreeNode); ok {
+				var tail *TreeNode
+				tail = v
+				node = tail.Right
+			}
+			stack.Remove(ele)
+		}
+	}
+
+	return res
+}
