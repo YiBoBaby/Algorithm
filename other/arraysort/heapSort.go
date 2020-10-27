9a5043853d84d70624bea851d890006fb9d204d0
ZhengYu <zhengy@gato.cn>
2020-10-27 13:12:38 +0800

    One question per day

--- other/arraysort/heapSort.go
+++ other/arraysort/heapSort.go
@@ -5,43 +5,45 @@ import "fmt"
 func main() {
 	arr := []int{91, 60, 96, 13, 35, 65, 46, 65, 10, 30, 20, 31, 77, 81, 22}
 	fmt.Println("排序前: ", arr)
+	heapSort(arr)
+	fmt.Println("排序后: ", arr)
+}
 
-	// 构建大顶堆
+func heapSort(arr []int) []int {
+	// 堆化
 	buildMaxHeap(arr)
-
-	// 遍历
 	for i := len(arr) - 1; i >= 0; i-- {
 		swap(arr, 0, i)
-		heapUp(arr, 0, i)
+		HeapUp(arr, 0, i-1)
 	}
-
-	fmt.Println("排序后: ", arr)
+	return arr
 }
 
 func buildMaxHeap(arr []int) {
 	for i := len(arr) / 2; i >= 0; i-- {
-		heapUp(arr, i, len(arr)-1)
+		HeapUp(arr, i, len(arr)-1)
 	}
 }
 
-// 堆化数组
-func heapUp(arr []int, nodeIndex, limitLen int) {
-	nodeChildL := 2*nodeIndex + 1
-	nodeChildR := 2*nodeIndex + 2
-	largestIndex := nodeIndex
+func HeapUp(arr []int, nonLeafNode, limit int) {
+	left := nonLeafNode*2 + 1
+	right := nonLeafNode*2 + 2
+	largest := nonLeafNode
 
-	if nodeChildL < limitLen && arr[largestIndex] < arr[nodeChildL] {
-		largestIndex = nodeChildL
+	if left <= limit && arr[left] > arr[largest] {
+		largest = left
 	}
-	if nodeChildR < limitLen && arr[largestIndex] < arr[nodeChildR] {
-		largestIndex = nodeChildR
+
+	if right <= limit && arr[right] > arr[largest] {
+		largest = right
 	}
-	if nodeIndex != largestIndex {
-		swap(arr, nodeIndex, largestIndex)
-		heapUp(arr, largestIndex, limitLen)
+
+	if nonLeafNode != largest {
+		swap(arr, largest, nonLeafNode)
+		HeapUp(arr, largest, limit)
 	}
 }
 
-func swap(arr []int, a, b int) {
-	arr[a], arr[b] = arr[b], arr[a]
+func swap(arr []int, i, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
 }
